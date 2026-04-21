daf7598903e92df5ed19eb14d75866dfddf16bfb
AmazingChris520 <55099375+AmazingChris520@users.noreply.github.com>
2026-04-21 05:26:55 +0000

    Maybe Works

--- uart_rx.sv
+++ uart_rx.sv
@@ -10,6 +10,11 @@ module uart_rx (
 parameter SRC_FREQ = 76800;
 parameter BAUDRATE = 9600;
 
+reg [1:0] state;
+reg [2:0] bit_index;
+reg [7:0] data_reg;
+reg uart_done;
+
 // STATES: State of the state machine
 localparam DATA_BITS = 8;
 localparam 
@@ -20,9 +25,65 @@ localparam
 
 // CLOCK MULTIPLIER: Instantiate the clock multiplier
 
+wire uart_clk;
+
+clock_mul #(
+    .SRC_FREQ(SRC_FREQ),
+    .OUT_FREQ(BAUDRATE)
+) clk_mul_inst (
+    .src_clk(clk),
+    .out_clk(uart_clk)
+);
+
 // CROSS CLOCK DOMAIN: The rx_ready flag should only be set 1 one for one source 
 // clock cycle. Use the cross clock domain technique discussed in class to handle this.
+reg sync_1 = 0, sync_2 = 0;
+
+always @(posedge clk) begin
+    sync_1 <= uart_done;
+    sync_2 <= sync_1;
+
+    // one-cycle pulse
+    rx_ready <= sync_1 & ~sync_2;
+
+    if (sync_1 & ~sync_2) begin
+        rx_data <= data_reg;
+    end
+end
 
 // STATE MACHINE: Use the UART clock to drive that state machine that receves a byte from the rx signal
+
+initial state = INIT;
+
+always @(posedge uart_clk) begin
+    case (state)
+        INIT: begin
+            state <= IDLE;
+            uart_done <= 0;
+        end
+
+        IDLE: begin
+            uart_done <= 0;
+            if (rx == 0) begin // start bit detected
+                bit_index <= 0;
+                state <= RX_DATA;
+            end
+        end
+
+        RX_DATA: begin
+            data_reg[bit_index] <= rx; // LSB first
+            if (bit_index == DATA_BITS-1) begin
+                state <= STOP;
+            end else begin
+                bit_index <= bit_index + 1;
+            end
+        end
+
+        STOP: begin
+            uart_done <= 1;
+            state <= IDLE;
+        end
+    endcase
+end
 
 endmodule
